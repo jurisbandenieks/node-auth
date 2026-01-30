84dd45b63d95edcf333c63e5f7c75a89a2736be0
Juris <jurisbandenieks90@gmail.com>
2026-01-30 14:01:36 -0800

    plainTextPassword

--- src/utils/password.ts
+++ src/utils/password.ts
@@ -1,14 +1,14 @@
 import { config } from "../config/index";
 import argon2 from "argon2";
 
-export async function hashPassword(plainText: string) {
-  const toHash = plainText + config.appPepper;
+export async function hashPassword(plainTextPassword: string) {
+  const toHash = plainTextPassword + config.appPepper;
 
   return argon2.hash(toHash, {
     type: argon2.argon2id,
   });
 }
-export async function verifyPassword(plainText: string, hash: string) {
-  const toVerify = plainText + config.appPepper;
+export async function verifyPassword(plainTextPassword: string, hash: string) {
+  const toVerify = plainTextPassword + config.appPepper;
   return argon2.verify(hash, toVerify);
 }
