f91b4f4d66778d14faae45653ec27a07138228b0
Juris <jurisbandenieks90@gmail.com>
2026-01-30 14:09:09 -0800

    user Service comments

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -3,13 +3,18 @@ import { redis } from "../config/redis";
 import { USER_PREFIX, USERID_PREFIX } from "../constants/redis";
 import { UserRecordModel } from "../types/user-model";
 
+// Find user by username
 export async function getUserByUsername(username: string): Promise<UserRecordModel | null> {
+  // Get user ID from username
   const id = await redis.get(`${USER_PREFIX}:${username}`);
   if (!id) return null;
+
+  // get user record data
   return getUserById(id);
 }
 
 export async function getUserById(id: string): Promise<UserRecordModel | null> {
+  // Get user data from DB by USERID_PREFIX
   const data = await redis.hGetAll(`${USERID_PREFIX}:${id}`);
   if (!data || Object.keys(data).length === 0) return null;
   return {
@@ -20,7 +25,9 @@ export async function getUserById(id: string): Promise<UserRecordModel | null> {
   };
 }
 
+// Create new user
 export async function createUser(username: string, passwordHash: string) {
+  // Generate unique ID for user
   const id = nanoid();
   const key = `${USERID_PREFIX}:${id}`;
   const now = new Date().toISOString();
